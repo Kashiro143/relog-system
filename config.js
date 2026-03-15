ba661d922afe0a10310f191364f25a18261078e3
Kashiro143 <kashirosakamaki@gmail.com>
2026-03-15 02:25:52 +0100

    Update version 5

---
+++ config.js
@@ -0,0 +1,16 @@
+// config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyA6KWfJaMKOyy8KzJCRS6Went1vx_4S_MQ", // À protéger idéalement
+    authDomain: "relog-system.firebaseapp.com",
+    databaseURL: "https://relog-system-default-rtdb.europe-west1.firebasedatabase.app/",
+    projectId: "relog-system",
+    appId: "1:869109824129:web:98565e8e25708461ef6b80"
+};
+
+export const defaultProfiles = {
+    Kashiro: { color: "#00d4ff", theme: "Cyber", font: "Courier New", msg: "[K-OS_SYSTEM_BREACH]" },
+    Saitho: { color: "#00ff41", theme: "Matrix", font: "monospace", msg: "> ARCHIVE_ACCESS" },
+    Foetic: { color: "#ff4500", theme: "Fantasy", font: "serif", msg: "🔥 FLAME_RESONANCE" },
+    Reindal: { color: "#d4af37", theme: "Minimal", font: "serif", msg: "🏔️ SOUFFLE MONTAGNE" },
+    Ximie: { color: "#ffb7d5", theme: "Cozy", font: "sans-serif", msg: "✨ NUANCE_CRÉATIVE" }
+};
